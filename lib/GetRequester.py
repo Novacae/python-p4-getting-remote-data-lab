6d54ec91e8c6a388a30143cb7f3d4c9263e7b51c
aman githinji <amangithinji09@gmail.com>
2023-12-13 15:48:28 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
+  
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        json_data = json.loads(self.get_response_body())
+        return json_data
